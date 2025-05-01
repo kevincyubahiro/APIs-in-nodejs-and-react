15ab2ae303761fab992d86716faaeb2e30b4e57d
rachel873 <racheliradukunda873@gmail.com>
2025-05-01 11:31:16 +0200

    APIs

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-import mysql from 'mysql'
-import cors from 'cors'
+import mysql from'mysql'
 import express from 'express'
-const app =express();
+import cors from 'cors'
+const app=express();
 app.use(express.json());
 app.use(cors());
-const db =mysql.createConnection({
+const db=mysql.createConnection({
     host:'localhost',
     user:'root',
     database:'node',
@@ -12,36 +12,51 @@ const db =mysql.createConnection({
 })
 db.connect((error)=>{
     if(error){
-        console.log('failed')
+        console.log("failed")
     }
     else{
-        console.log("connected")
+        console.log('yes connected')
     }
 })
+//select
 app.get('/select',(req,res)=>{
-    const sql ="SELECT  * FROM student"
+    const sql ="SELECT * FROM student"
     db.query(sql,(error,result)=>{
         if(error) return res.status(500).json('failed')
-            return res.status(200).json('seleted')
+            return res.status(200).json(result)
     })
 })
+//insert
 app.post('/insert',(req,res)=>{
     const {username,password}=req.body;
-    const sql ="INSERT INTO student (username,password)VALUES(?,?)"
+    const sql="INSERT INTO student (username,password)VALUES(?,?)";
     db.query(sql,[username,password],(error,result)=>{
         if(error) return res.status(500).json('failed')
-            return res.status(200).json('succussfully')
-    })
-    //delete
-    app.delete('/delete/:id',(req,res)=>{
-        const{id}=req.params;
-        const sql ="DELETE FROM student WHERE id =?";
-        db.query(sql,[id],(error,result)=>{
-            if(error) return res.status(500).json('failed')
-                return res.status(200).json('deleted')
-        })
+            return res.status(200).json(result)
+        
     })
 })
-app.listen(2000,()=>{
-    console.log("running http://localhost:2000")
+//delete
+app.delete('/delete/:id',(req,res)=>{
+    const{id}=req.params;
+    const sql="DELETE FROM student WHERE id=?";
+    db.query(sql,[id],(error,result)=>{
+        if(error) return res.status(500).json('failed')
+            return res.status(200).json(result)
+
+    })
+    
+
+})
+app.put('/update/:id',(req,res)=>{
+    const {id}=req.params;
+    const {username,password}=req.body;
+    const sql="UPDATE student SET username=?,password=? WHERE id=?";
+    db.query(sql,[username,password,id],(error,result)=>{
+        if(error) return res.status(500).json("failed")
+            return res.status(200).json('inserted')
+    })
+})
+app.listen(3000,()=>{
+    console.log('app running on port htpp://localhost:3000')
 })
